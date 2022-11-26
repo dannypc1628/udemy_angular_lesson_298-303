a5788e23a79a0696f79d66fe0c738df1cb02da38
dannyLiu <h152074716@gmail.com>
2022-11-26 15:41:27 +0800

    299-1 建立 user.model.ts ， 登入成功後儲存 userData

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -1,7 +1,8 @@
 import { Injectable } from "@angular/core";
 import { HttpClient, HttpErrorResponse } from "@angular/common/http";
-import { catchError } from "rxjs/operators";
-import { throwError } from "rxjs";
+import { catchError, tap } from "rxjs/operators";
+import { Subject, throwError } from "rxjs";
+import { User } from "./user.model";
 
 export interface AuthResponseData {
   kind: string;
@@ -15,6 +16,8 @@ export interface AuthResponseData {
 
 @Injectable({ providedIn: "root" })
 export class AuthService {
+  user = new Subject<User>();
+
   constructor(private http: HttpClient) {}
 
   signup(email: string, password: string) {
@@ -40,7 +43,21 @@ export class AuthService {
           returnSecureToken: true,
         }
       )
-      .pipe(catchError(this.handleError));
+      .pipe(
+        catchError(this.handleError),
+        tap((resData) => {
+          const expirationDate = new Date(
+            new Date().getTime() + +resData.expiresIn * 1000
+          );
+          const user = new User(
+            resData.email,
+            resData.localId,
+            resData.idToken,
+            expirationDate
+          );
+          this.user.next(user);
+        })
+      );
   }
 
   private handleError(errorRes: HttpErrorResponse) {
