8aa29a62d743d7bccc3dfa49e9ec89878207bc11
dannyLiu <h152074716@gmail.com>
2022-11-26 18:59:34 +0800

    302-2 : 使用 auth-interceptor.service.ts

--- src/app/auth/auth-interceptor.service.ts
+++ src/app/auth/auth-interceptor.service.ts
@@ -16,6 +16,10 @@ export class AuthInterceptorService implements HttpInterceptor {
     return this.authService.user.pipe(
       take(1),
       exhaustMap((user) => {
+        if (!user) {
+          return next.handle(req);
+        }
+
         const modifiedReq = req.clone({
           params: new HttpParams().set("auth", user.token),
         });
